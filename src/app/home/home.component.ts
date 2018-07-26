d8a3b62153b38e860d7b211d108349ef1bbc0881
Thiago Roque <tg18roque@gmail.com>
2018-07-26 19:09:45 -0300

    Retorno de fila correto.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Form } from '../models/form';
+import { FormResult } from '../models/formResult';
 import { Observable, of } from "rxjs";
+import { JsonPipe } from '../../../node_modules/@angular/common';
 
 @Component({
   selector: 'app-home',
@@ -12,14 +14,18 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   form:Form = new Form();  
-  forms: Form[] = [];
+  
+  formResult:FormResult = new FormResult();  
+  forms: FormResult[] = [];
+  returnData:any;
 
+  jsonVar:JsonPipe;
+  
   ngOnInit() {
     this.form.id = 1;
   }
 
   onSubmit(){
-    console.log(this.form);
     fetch('http://localhost:63864/api/Form/' , {
       method: 'POST',
       headers: {
@@ -30,10 +36,28 @@ export class HomeComponent implements OnInit {
         form: this.form
       })
     })
-    .then((data) => {
-      this.forms.push(this.form);
-      this.NewFields();
-    })
+    .then(
+      data => {
+        this.returnData = data;
+        var text; 
+        var p3 = Promise.resolve(this.returnData.json());
+        var teste = p3.then((v) => {
+          this.formResult.id = v["Id"];
+          this.formResult.msgs = v["Msgs"];
+          this.formResult.price = v["Price"];
+          this.formResult.quantity = v["Quantity"];
+          this.formResult.side = v["Side"];
+          this.formResult.status = v["Status"];
+          this.formResult.symbol = v["Symbol"];
+          
+          this.forms.push(this.formResult);
+          this.NewFields();
+
+        }, function(e) {
+          console.log(e);
+        })
+      }      
+    )
     .catch((error) => {
       console.error(error);
     })
@@ -41,6 +65,7 @@ export class HomeComponent implements OnInit {
 
   NewFields(){
     this.form = new Form();
+    this.formResult = new FormResult();
     this.form.id = this.forms.length + 1;
   }
 
---
+++ src/app/models/formResult.ts
@@ -0,0 +1,9 @@
+export class FormResult {
+    id:number;
+    side:string;
+    symbol:string;
+    quantity:number;
+    price:number;
+    status:string;
+    msgs: string;
+}
